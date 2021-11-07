ece1a736bbab9800cfb48dea87394719bb94d4c7
fregean <yeshooah@gmail.com>
2021-11-07 12:35:09 +0900

    fix batch.py

--- batch.py
+++ batch.py
@@ -4,26 +4,30 @@ from email.utils import formatdate
 
 import config
 
-sendAddress = config.MAIL_ADDRESS
-password = config.MAIL_PASSWORD
+def send_email():
+    sendAddress = config.MAIL_ADDRESS
+    password = config.MAIL_PASSWORD
 
-subject = 'ごきげんいかがですか'
-bodyText = 'いつもおつかれさまです'
-fromAddress = sendAddress
-toAddress = 'yuki_fujisawa@wywy.jp'
+    subject = 'ごきげんいかがですか'
+    bodyText = 'いつもおつかれさまです'
+    fromAddress = sendAddress
+    toAddress = 'yuki_fujisawa@wywy.jp'
 
-# SMTPサーバに接続
-smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpobj.starttls()
-smtpobj.login(sendAddress, password)
+    # SMTPサーバに接続
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.starttls()
+    smtpobj.login(sendAddress, password)
 
-# メール作成
-msg = MIMEText(bodyText)
-msg['Subject'] = subject
-msg['From'] = fromAddress
-msg['To'] = toAddress
-msg['Date'] = formatdate()
+    # メール作成
+    msg = MIMEText(bodyText)
+    msg['Subject'] = subject
+    msg['From'] = fromAddress
+    msg['To'] = toAddress
+    msg['Date'] = formatdate()
 
-# 作成したメールを送信
-smtpobj.send_message(msg)
-smtpobj.close()
+    # 作成したメールを送信
+    smtpobj.send_message(msg)
+    smtpobj.close()
+
+if __name__ == "__main__":
+    send_email.run()
