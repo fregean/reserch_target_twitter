84ac7e0b53dffa13d35d3419fccf089b253ba8db
fregean <yeshooah@gmail.com>
2021-11-10 13:17:56 +0900

    delete ndjson

--- app.py
+++ app.py
@@ -102,9 +102,13 @@ def post_tweets():
 
 @app.route('/display_information', methods=['GET'])
 def display_information():
+
+    DIR='data/db_tweets.ndjson'
     twitter_id = request.args.get('twitter_id')
     session['client']['twitter_id'] = twitter_id
-    with open('data/db_tweets.ndjson', mode='a') as f:
+    if not os.path.exists(DIR):
+        os.mkdir(DIR)
+    with open(DIR, mode='a') as f:
         writer = ndjson.writer(f)
         writer.writerow(session['client'])
     return render_template('display_information.html')
